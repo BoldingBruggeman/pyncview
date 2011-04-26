9d8b7d21aaa2bd126c33d53c49ef52bf831945e1
Jorn Bruggeman <jorn@bolding-burchard.com>
2011-04-26 10:10:18 +0000

    added ordereddict dependency to setup (netCDF4, Python <2.7 only)

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     options={'py2exe': {
                 'packages' : ['matplotlib', 'pytz'],
 #                'includes' : ['sip','PyQt4._qt'],
-                'includes' : ['sip','netCDF4_utils','netcdftime'],
+                'includes' : ['sip','netCDF4_utils','netcdftime','ordereddict'],
                 'excludes' : ['_gtkagg', '_tkagg', '_wxagg','Tkconstants','Tkinter','tcl','wx','pynetcdf','cProfile','pstats','modeltest','pupynere','Scientific'],
                 'dll_excludes': ['libgdk-win32-2.0-0.dll', 'libgobject-2.0-0.dll', 'libgdk_pixbuf-2.0-0.dll','wxmsw26uh_vc.dll','tcl84.dll','tk84.dll','powrprof.dll'],
             }},
