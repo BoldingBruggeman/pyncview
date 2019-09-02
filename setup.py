3ba9125f25040e3eefedca8d8cad68813f735851
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2019-09-02 21:35:47 +0200

    version boost

--- setup.py
+++ setup.py
@@ -88,13 +88,13 @@ else:
     from setuptools import setup
 
     setup(name='pyncview',
-        version='0.99.18',
+        version='0.99.19',
         description='NetCDF viewer written in Python',
         url='http://github.com/BoldingBruggeman/pyncview',
         author='Jorn Bruggeman',
         author_email='jorn@bolding-bruggeman.com',
         license='GPL',
-        install_requires=['xmlplot>=0.9.12'],
+        install_requires=['xmlplot>=0.9.13'],
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
