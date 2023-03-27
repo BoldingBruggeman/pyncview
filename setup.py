47ca659cdc2e5d1a610e5941eafd6b11d8498417
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-03-27 08:50:28 +0100

    updated xmlplot

--- setup.py
+++ setup.py
@@ -88,13 +88,13 @@ else:
     from setuptools import setup
 
     setup(name='pyncview',
-        version='0.99.33',
+        version='0.99.34',
         description='NetCDF viewer written in Python',
         url='http://github.com/BoldingBruggeman/pyncview',
         author='Jorn Bruggeman',
         author_email='jorn@bolding-bruggeman.com',
         license='GPL',
-        install_requires=['xmlplot>=0.9.25'],
+        install_requires=['xmlplot>=0.9.26'],
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
